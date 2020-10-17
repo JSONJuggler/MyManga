91b880526f4984c577f1dd1a575a32c99992d05e
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-10-16 20:40:28 -0500

    Using ip address as base url for dev env requests

--- src/actions/manga.ts
+++ src/actions/manga.ts
@@ -1,12 +1,13 @@
 import { Dispatch } from "redux";
 import { create } from "apisauce";
 import { SearchResult, Manga, SEARCH_MANGA, SAVE_MANGA, MangaActionTypes } from "./types";
+import Config from "react-native-config"
 
 export const searchManga = (searchQuery: string) => async (dispatch: Dispatch) => {
   try {
     // const res = await axios.get("/recipemanager/api/recipes/me");
     const api = create({
-      baseURL: "http://localhost:5000",
+      baseURL: Config.BASE_URL,
     })
 
     const { data } = await api.get("/api/manga/search?manga=" + searchQuery)
